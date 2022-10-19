f29a5a7ccfcee312885a62ec9050f48fb8e4d445
stenio heck <106745890+stenioheck@users.noreply.github.com>
2022-10-19 09:20:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,153 @@
+// imagens e sons do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons do jogo
+
+let som;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  imagemDaEstrada = loadImage("estrada.png");
+  imagemDoAtor = loadImage("ator-1.png");
+  imagemCarro = loadImage("carro-1.png");
+  imagemCarro2 = loadImage("carro-2.png");
+  imagemCarro3 = loadImage("carro-3.png");  
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+  
+  som = loadSound("trilha.mp3");
+  somDaColisao = loadSound("colidiu.mp3");
+  somDoPonto = loadSound("pontos.wav");
+}
+
+//código do ator
+let xAtor = 85;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if(podeSeMover()){
+      yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      voltaAtorParaPosicaoInicial();
+      somDaColisao.play();
+      if (pontosMaiorQueZero()){
+        meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60))
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorParaPosicaoInicial();
+    velocidadeCarros = [getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1)];
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover(){
+  return yAtor < 366;
+}
+
+//código do carro
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1), getRandomArbitrary(1, meusPontos+1)];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < - 50;
+}
+collideRectCircle = function (rx, ry, rw, rh, cx, cy, diameter) {
+  //2d
+  // temporary variables to set edges for testing
+  var testX = cx;
+  var testY = cy;
+
+  // which edge is closest?
+  if (cx < rx){         testX = rx       // left edge
+  }else if (cx > rx+rw){ testX = rx+rw  }   // right edge
+
+  if (cy < ry){         testY = ry       // top edge
+  }else if (cy > ry+rh){ testY = ry+rh }   // bottom edge
+
+  // // get distance from closest edges
+  var distance = this.dist(cx,cy,testX,testY)
+
+  // if the distance is less than the radius, collision!
+  if (distance <= diameter/2) {
+    return true;
+  }
+  return false;
+};
+
+
 function setup() {
   createCanvas(500, 400);
-  somDaTrilha.loop();
+  som.loop();
 }
 
 function draw() {
